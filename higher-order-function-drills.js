f9c2529c123d7587df2efcbb5ea8a333a28da93c
rpinto <rachel.lea.pinto@gmail.com>
2018-06-12 13:59:32 -0700

    third exercise

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -31,35 +31,51 @@
 // -----------------------------------------------
 
 
-// DO NOT EDIT BETWEEN THESE LINES ----->
-// Return only names that begin with 'R'
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// // DO NOT EDIT BETWEEN THESE LINES ----->
+// // Return only names that begin with 'R'
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filteredNames = filter(myNames, function(name) {
-    // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'R';
-});
+// const filteredNames = filter(myNames, function(name) {
+//     // This is a "predicate function" - it's a function that only returns a boolean
+//     return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
 
-// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+// // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
 
 
-function filter(arr, fn) {
-	let newArray = [];
-	for (var i = 0; i < arr.length; i++) {
-		if (fn(arr[i])) {
-			newArray.push(arr[i] === true);
-		}
-	}
-	return newArray;
+// function filter(arr, fn) {
+// 	let newArray = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		if (fn(arr[i])) {
+// 			newArray.push(arr[i] === true);
+// 		}
+// 	}
+// 	return newArray;
+// }
+
+// console.log(filter(myNames, filter));
+
+
+// -----------------------------------------------
+ 
+function hazardWarningCreator(typeOfWarning) {
+    let warningCounter = 0;
+
+    return function(location) {
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
+    }
+
 }
+    const rocksWarning = hazardWarningCreator('Rocks on the Road');
+    const deerAhead = hazardWarningCreator('Look out for deer');
+    const hairpinTurn = hazardWarningCreator('Drive slow, turns ahead'); 
 
-console.log(filter(myNames, filter));
-
+deerAhead("Canada");
+deerAhead("Canada");
 
  -----------------------------------------------
-
-
-
