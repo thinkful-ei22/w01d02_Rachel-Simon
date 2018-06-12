f3604e6a6539fb6eb30c0a3ff377459e362a6fd7
Simon Chae <s13chae@gmail.com>
2018-06-12 12:12:30 -0700

    loops and arrays drill

---
+++ loops-array-drill.js
@@ -0,0 +1,86 @@
+function max(numbers) {
+  let i = 0;
+  let currentMax = numbers[0];
+  while (i < numbers.length) {
+    i++;
+    if (numbers[i] > currentMax) {
+      currentMax = numbers[i];
+    }
+  }
+  return currentMax;
+}
+
+console.log(max([1, 5 ,6 ,3]));
+
+function min(numbers) {
+  let i = 1;
+  let currentMin = numbers[0];
+  while (i < numbers.length) {
+    i++;
+    if (numbers[i] < currentMin) {
+      currentMin = numbers[i];
+    }
+  }
+  return currentMin;
+}
+
+console.log(min([1, 5 ,6 ,3]));
+
+
+
+// tests
+
+// function testFunctionWorks(fn, input, expected) {
+//   if (fn(input) === expected) {
+//     console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+//     return true;
+//   } else {
+//     console.log(
+//       'FAILURE: `' +
+//         fn.name +
+//         '([' +
+//         input +
+//         '])` should be ' +
+//         expected +
+//         ' but was ' +
+//         fn(input)
+//     );
+//     return false;
+//   }
+// }
+
+// function testEmpty(fn) {
+//   if (fn([]) === null || fn([]) == undefined) {
+//     console.log(`SUCCESS: ${fn.name} works on empty arrays`);
+//     return true;
+//   } else {
+//     console.log(
+//       `FAILURE: ${fn.name} should return undefined or null for empty arrays`
+//     );
+//     return false;
+//   }
+// }
+
+// (function runTests() {
+//   // we'll use the variables in our test cases
+//   const numList1 = [-5, 28, 98, -20013, 0.7878, 22, 115];
+//   const realMin1 = numList1[3];
+//   const realMax1 = numList1[6];
+//   const numList2 = [0, 1, 2, 3, 4];
+//   const realMin2 = numList2[0];
+//   const realMax2 = numList2[4];
+
+//   const testResults = [
+//     testFunctionWorks(max, numList1, realMax1),
+//     testFunctionWorks(max, numList2, realMax2),
+//     testFunctionWorks(min, numList1, realMin1),
+//     testFunctionWorks(min, numList2, realMin2),
+//     testEmpty(max),
+//     testEmpty(min),
+//   ];
+
+//   const numPassing = testResults.filter(function(result) {
+//     return result;
+//   }).length;
+//   console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
+// })();
