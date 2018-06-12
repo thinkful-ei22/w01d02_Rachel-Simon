7042903cdee27ae0db44ce44f9460502d10c6cd6
rpinto <rachel.lea.pinto@gmail.com>
2018-06-12 12:49:59 -0700

    created compute average function

---
+++ compute-the-average.js
@@ -0,0 +1,12 @@
+
+
+function average(numbers) {
+    let sum = 0;
+    //for each item in our array, iterate through numbers and find the sum(average)
+    numbers.forEach(item => sum += item) 
+    //arrow function allows us to name each individual item in the array
+    //and apply a condition function to it
+    return sum / numbers.length;
+}
+
+console.log(average([2,2,2]));
